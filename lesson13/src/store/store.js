5e003a0307ac59ec453c7826b1fa1f445543415a
echoan <1141272923@qq.com>
2019-06-26 17:50:54 +0800

    vuex学习过程演示代码

--- lesson13/src/store/store.js
+++ lesson13/src/store/store.js
@@ -1,15 +1,13 @@
 import Vuex from 'vuex'
 import mutations from './mutations'
 import state from './modules/shop'
-import actions from './actions'
 import getters from './getters'
 
 function CreateStore(){
     return new Vuex.Store({
         state,
-        actions,
         getters,
-        mutations,
+        mutations
     })
 }
 //导出创建的仓库
